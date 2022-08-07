2d483c56aef94f01237efea13fce22b5579077cd
shivamkrsingh25 <110761389+shivamkrsingh25@users.noreply.github.com>
2022-08-08 02:47:40 +0530

    Add files via upload

---
+++ Basic/Intro.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a=50,b=40,c;
+    c = a + b;
+cout<<c;
+return 0;
+}
---
+++ Basic/incrementq2.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a = 1;
+    int b = 2;
+    if (a-- > 0 && ++b < 2)
+    {
+       cout << "Stage 1 - Inside if" << endl;
+    }
+    else
+    {
+        cout << "Stage 2 - Inside else" << endl;
+    }
+    cout << a << " " << b << endl;
+    return 0;
+}
