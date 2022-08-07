885d590298a629fa36b6f9746d3dee94a3278cff
shivamkrsingh25 <110761389+shivamkrsingh25@users.noreply.github.com>
2022-08-08 03:05:56 +0530

    Delete Basic directory

--- Basic/Intro.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-main()
-{
-    int a=50,b=40,c;
-    c = a + b;
-cout<<c;
-return 0;
-}
--- Basic/incrementq4.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> 
-using namespace std;
-int main(int argc, char const *argv[])
-{
-    int num = 3;
-    cout << ( 25 * (++num));
-    return 0;
-}
